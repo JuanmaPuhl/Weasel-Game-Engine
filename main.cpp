1875f8fb108dfe0e55786640b5dc574404c2f005
Juan Manuel Puhl <43391912+JuanmaPuhl@users.noreply.github.com>
2020-11-24 11:00:40 -0300

    first window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,68 @@
-#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#define WIDTH 1280
+#define HEIGHT 720
 
-int main()
+/*Declaraciplayers previas de funciplayers implementadas mas abajo*/
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+float currentFrame = 0.0f;
+int main(void)
 {
-    printf("Hello, World!\n");
+  /*=====================INICIA CREACION DE VENTANA==========================*/
+  GLFWwindow* window;
+  if (!glfwInit())
+  {
+    return -1;
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  window = glfwCreateWindow(WIDTH, HEIGHT, "Game", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+  glfwSetKeyCallback(window, key_callback);
+  glfwMakeContextCurrent(window);
+  glewExperimental = GL_TRUE;
+  glewInit();
+  glGetError();
+
+  /*====================TERMINA CREACION DE VENTANA==========================*/
+  glViewport(0, 0, WIDTH, HEIGHT);
+  glEnable(GL_BLEND);
+  glEnable(GL_DEPTH_TEST);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  GLfloat deltaTime = 0.0f;
+  GLfloat lastFrame = 0.0f;
+  while (!glfwWindowShouldClose(window))
+  {
+    currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+  return 0;
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  }
+  if (key >= 0 && key < 1024)
+  {
+    /*
+    if (action == GLFW_PRESS)
+        Game.Keys[key] = GL_TRUE;
+    else if (action == GLFW_RELEASE)
+        Game.Keys[key] = GL_FALSE;
+    */
+  }
 }
--- makefile
+++ makefile
@@ -1,2 +1,4 @@
 main:main.cpp
-	gcc -o main main.cpp
+	gcc -o main.exe -I ./libs/ main.cpp -L ./libs/ -lglew32 -lglfw3 -lopengl32
+clean:
+	del main.exe
