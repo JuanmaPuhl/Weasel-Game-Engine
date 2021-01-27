937db746e9b8b886f5ab212d976cf0e5d81772da
Juan Manuel Puhl <43391912+JuanmaPuhl@users.noreply.github.com>
2021-01-27 14:48:11 -0300

    Update WindowsDialogs.h

--- source/Utils/WindowsDialogs.h
+++ source/Utils/WindowsDialogs.h
@@ -5,6 +5,12 @@
 #include <string>
 namespace Utils
 {
+    /**
+     * @brief Abre un dialogo de windows para abrir un archivo
+     * 
+     * @param toReturn Es el buffer en el que se guardará la dirección elegida por el usuario
+     * @param size El tamaño máximo del buffer ingresado
+     */
     void openFileDialog(TCHAR* toReturn, int size)
     {
         OPENFILENAME ofn;       // common dialog box structure
@@ -12,10 +18,10 @@ namespace Utils
         // Initialize OPENFILENAME
         ZeroMemory(&ofn, sizeof(ofn));
         ofn.lStructSize = sizeof(ofn);
-        ofn.hwndOwner = NULL;
+        ofn.hwndOwner = NULL; //Despues cambiar por el handler de la ventana del engine para que quede bloqueado
         ofn.lpstrFile = toReturn;
         ofn.nMaxFile = size;
-        ofn.lpstrFilter = _T("Text Files\0*.txt\0Any File\0*.*\0");
+        ofn.lpstrFilter = _T("Text Files\0*.txt\0Any File\0*.*\0"); //Filtro de tipo de archivo
         ofn.nFilterIndex = 1;
         ofn.lpstrFileTitle = NULL;
         ofn.nMaxFileTitle = 0;
@@ -30,6 +36,12 @@ namespace Utils
             toReturn = NULL;
     }
 
+    /**
+     * @brief Abre un dialogo de windows para guardar un archivo
+     * 
+     * @param toReturn Es el buffer en el que se guardará la dirección elegida por el usuario
+     * @param size El tamaño máximo del buffer ingresado
+     */
     void saveFileDialog(TCHAR* toReturn, int size)
     {
         OPENFILENAME ofn;       // common dialog box structure
@@ -37,10 +49,10 @@ namespace Utils
         // Initialize OPENFILENAME
         ZeroMemory(&ofn, sizeof(ofn));
         ofn.lStructSize = sizeof(ofn);
-        ofn.hwndOwner = NULL;
+        ofn.hwndOwner = NULL; //Despues cambiar por el handler de la ventana del engine para que quede bloqueado
         ofn.lpstrFile = toReturn;
         ofn.nMaxFile = size;
-        ofn.lpstrFilter = _T("Text Files\0*.txt\0Any File\0*.*\0");
+        ofn.lpstrFilter = _T("Text Files\0*.txt\0Any File\0*.*\0"); //Filtro de tipo de archivo
         ofn.nFilterIndex = 1;
         ofn.lpstrFileTitle = NULL;
         ofn.nMaxFileTitle = 0;
