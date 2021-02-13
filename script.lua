80075ededf12addbaf26135bfc5df2d91d3e5d2a
Juan Manuel Puhl <43391912+JuanmaPuhl@users.noreply.github.com>
2021-02-13 18:35:19 -0300

    ejecutando LUA

--- script.lua
+++ script.lua
@@ -1,5 +1,3 @@
-a = {}
-print("El tipo de la variable a es: ",type(a))
-a.name = "Entity 1"
-a.id = "AXINGEI35g33hs9s"
-print("Name: ",a.name," Id: ",a.id)
+-- Create an instance of sprite an call some methods
+print("HOLI")
+position()
